b6c890c4de7dc263d3e13c380394a8516a44dded
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2013-03-24 19:05:34 +0100

    Bumped version to 0.3.0-SNAPSHOT

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ Setup script for PyPi
 from setuptools import setup
 
 setup(name='dynamic-dynamodb',
-    version='0.2.0',
+    version='0.3.0-SNAPSHOT',
     license='Apache License, Version 2.0',
     description='Automatic provisioning for AWS DynamoDB tables',
     author='Sebastian Dahlgren',
