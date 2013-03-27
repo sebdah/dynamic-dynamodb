230621ffdc01501c167458563c69e31697488074
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2013-03-27 15:01:38 +0100

    Bumped version to 0.4.0-SNAPSHOT

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 
 setup(name='dynamic-dynamodb',
-    version='0.3.0',
+    version='0.4.0-SNAPSHOT',
     license='Apache License, Version 2.0',
     description='Automatic provisioning for AWS DynamoDB tables',
     author='Sebastian Dahlgren',
