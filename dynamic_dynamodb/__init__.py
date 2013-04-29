ea4a672681f72361918d3c2434ea3b2d0fd3c87a
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2013-04-29 10:13:50 +0200

    Bumped version to 1.3.0-SNAPSHOT

--- dynamic_dynamodb/__init__.py
+++ dynamic_dynamodb/__init__.py
@@ -27,7 +27,7 @@ import core
 from daemon import Daemon
 from config_handler import CONFIGURATION as configuration
 
-VERSION = '1.2.5'
+VERSION = '1.3.0-SNAPSHOT'
 
 
 class DynamicDynamoDBDaemon(Daemon):
