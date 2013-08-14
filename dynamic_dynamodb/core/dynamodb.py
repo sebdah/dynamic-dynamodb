1ebaea41d762ab1b531d3501b1edf6497f24288e
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2013-08-14 19:45:47 +0200

    Added functionality for retrying failed connections #51

--- dynamic_dynamodb/core/dynamodb.py
+++ dynamic_dynamodb/core/dynamodb.py
@@ -1,5 +1,6 @@
 """ Handle most tasks related to DynamoDB interaction """
 import sys
+import time
 
 from boto import dynamodb
 from boto.exception import DynamoDBResponseError
@@ -8,26 +9,38 @@ from dynamic_dynamodb.log_handler import LOGGER as logger
 from dynamic_dynamodb.config_handler import CONFIGURATION as configuration
 
 
-def __get_connection_dynamodb():
-    """ Ensure connection to DynamoDB """
-    try:
-        if (configuration['global']['aws_access_key_id'] and
-            configuration['global']['aws_secret_access_key']):
-            connection = dynamodb.connect_to_region(
-                configuration['global']['region'],
-                aws_access_key_id=configuration['global']['aws_access_key_id'],
-                aws_secret_access_key=\
-                    configuration['global']['aws_secret_access_key'])
-        else:
-            connection = dynamodb.connect_to_region(
-                configuration['global']['region'])
+def __get_connection_dynamodb(retries=3):
+    """ Ensure connection to DynamoDB
 
-    except Exception as err:
-        logger.error('Failed connecting to DynamoDB: {0}'.format(err))
-        logger.error(
-            'Please report an issue at: '
-            'https://github.com/sebdah/dynamic-dynamodb/issues')
-        raise
+    :type retries: int
+    :param retries: Number of times to retry to connect to DynamoDB
+    """
+    connected = False
+    while not connected:
+        try:
+            if (configuration['global']['aws_access_key_id'] and
+                configuration['global']['aws_secret_access_key']):
+                connection = dynamodb.connect_to_region(
+                    configuration['global']['region'],
+                    aws_access_key_id=configuration['global']['aws_access_key_id'],
+                    aws_secret_access_key=\
+                        configuration['global']['aws_secret_access_key'])
+            else:
+                connection = dynamodb.connect_to_region(
+                    configuration['global']['region'])
+            connected = True
+
+        except Exception as err:
+            logger.error('Failed to connect to DynamoDB: {0}'.format(err))
+            if retries == 0:
+                logger.error(
+                    'Please report an issue at: '
+                    'https://github.com/sebdah/dynamic-dynamodb/issues')
+                raise
+            else:
+                logger.error('Retrying in 5 seconds')
+                retries -= 1
+                time.sleep(5)
 
     logger.debug('Connected to DynamoDB')
     return connection
