91d15a38c799c23431589a093e917280589627a2
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2013-08-22 08:18:08 +0200

    Bumped version to 1.5.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 
 setup(name='dynamic-dynamodb',
-    version='1.5.0-SNAPSHOT',
+    version='1.5.0',
     license='Apache License, Version 2.0',
     description='Automatic provisioning for AWS DynamoDB tables',
     author='Sebastian Dahlgren',
